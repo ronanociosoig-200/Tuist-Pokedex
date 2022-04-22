cdfb90247831523b430d566bd07f8ef1bcc6cd72
Ronan O Ciosoig <ronanoc@sonomos.com>
2022-04-22 16:26:52 +0200

    Trigger CI

--- Features/Pokedex/Sources/Core/AppDelegate.swift
+++ Features/Pokedex/Sources/Core/AppDelegate.swift
@@ -16,9 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         disableAnimations()
-        
         appController.start()
-        
         return true
     }
     
