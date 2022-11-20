fe1170423a83ea3e00d03df201d165a6e0788e26
Ronan O Ciosoig <ronanoc@sonomos.com>
2022-11-21 00:26:41 +0100

    Add tests covering backpack wireframe

--- Features/Backpack/Tests/BackpackTests.swift
+++ Features/Backpack/Tests/BackpackTests.swift
@@ -1,8 +1,40 @@
 import Foundation
 import XCTest
+@testable import Backpack
+@testable import Common
 
-final class Backpack_UITests: XCTestCase {
-    func test_example() {
-        XCTAssertEqual("BackpackUI", "BackpackUI")
+final class BackpackTests: XCTestCase {
+    func testViewControllerHasInjectedStructure() {
+        let vc = BackpackWireframe.makeViewController()
+        
+        XCTAssertNotNil(vc, "ViewController should not be nil")
+        
+        BackpackWireframe.prepare(vc, actions: MockActions(), dataProvider: MockDataProvider())
+        
+        let presenter = vc.presenter
+        
+        XCTAssertNotNil(presenter, "Presenter must not be nil")
+        
+        XCTAssertNotNil(presenter?.delegate, "Presenter delegate must not be nil")
+        
+        XCTAssertNotNil(presenter?.dataSource, "Presenter data source must not be nil")
+    }
+    
+    func testNavigationControllerNotNil() {
+        let navController = BackpackWireframe.makeNavigationController()
+        
+        XCTAssertNotNil(navController, "NavigationController should not be nil")
+    }
+}
+
+struct MockActions: BackpackActions {
+    func selectItem(at index: Int) {
+        
+    }
+}
+
+struct MockDataProvider: BackpackDataProvider {
+    func pokemons() -> [Common.LocalPokemon] {
+        return []
     }
 }
