b1a5bd03900c4afc3f5c4465ee8e919891052d2a
Ronan O Ciosoig <ronan.ociosoig@edreamsodigeo.com>
2023-05-20 23:29:52 +0200

    Move commands to compress simulator builds to a dedicated shell script and split out the run step in the config.yml

---
+++ scripts/compressSimulatorBuilds.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "Compress simulator builds in Derived Data if it exists. Exclude test runners. "
+echo "Copy each .app to simulatorApps"
+find /Users/distiller/Library/Developer/Xcode/DerivedData -name "*.app" | grep -v "Tests-Runner" | while read in; do tar -cvzf "$in".tar.gz -C "$in" .; done
+mkdir "simulatorApps"
+echo "mkdir simulatorApps"
+find /Users/distiller/Library/Developer/Xcode/DerivedData -name "*.app.tar.gz" | while read in; do cp "$in" simulatorApps; done
+ls -l simulatorApps
+echo "Done"
