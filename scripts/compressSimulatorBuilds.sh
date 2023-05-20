723d53bab25213ece8df1f0b13a9623ab7d39764
Ronan O Ciosoig <ronan.ociosoig@edreamsodigeo.com>
2023-05-20 23:37:09 +0200

    Check if dir exists to run commands

--- scripts/compressSimulatorBuilds.sh
+++ scripts/compressSimulatorBuilds.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 echo "Compress simulator builds in Derived Data if it exists. Exclude test runners. "
 echo "Copy each .app to simulatorApps"
-find /Users/distiller/Library/Developer/Xcode/DerivedData -name "*.app" | grep -v "Tests-Runner" | while read in; do tar -cvzf "$in".tar.gz -C "$in" .; done
-mkdir "simulatorApps"
-echo "mkdir simulatorApps"
-find /Users/distiller/Library/Developer/Xcode/DerivedData -name "*.app.tar.gz" | while read in; do cp "$in" simulatorApps; done
-ls -l simulatorApps
-echo "Done"
+if [ -d /Users/distiller/Library/Developer/Xcode/DerivedData ]; then
+  find /Users/distiller/Library/Developer/Xcode/DerivedData -name "*.app" | grep -v "Tests-Runner" | while read in; do tar -cvzf "$in".tar.gz -C "$in" .; done
+  mkdir "simulatorApps"
+  echo "mkdir simulatorApps"
+  find /Users/distiller/Library/Developer/Xcode/DerivedData -name "*.app.tar.gz" | while read in; do cp "$in" simulatorApps; done
+  ls -l simulatorApps
+  echo "Done"
+fi
+
