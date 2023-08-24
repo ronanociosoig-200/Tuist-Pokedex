b03de5df4b39933ac04224b185cbc926c4ecae0a
Ronan O Ciosoig <ronan.ociosoig@edreamsodigeo.com>
2023-08-24 17:38:46 +0200

    Re-enable UI testing

--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -278,19 +278,19 @@ extension Project {
                 .target(name: "\(name)")
             ])
 
-//        let uiTestTarget = Target(
-//            name: "\(name)UITests",
-//            platform: platform,
-//            product: .uiTests,
-//            bundleId: "\(reverseOrganizationName).\(name)UITests",
-//            infoPlist: .default,
-//            sources: ["\(appPath)/\(name)/UITests/**"],
-//            resources: [],
-//            dependencies: [
-//                .target(name: "\(name)")
-//            ])
+        let uiTestTarget = Target(
+            name: "\(name)UITests",
+            platform: platform,
+            product: .uiTests,
+            bundleId: "\(reverseOrganizationName).\(name)UITests",
+            infoPlist: .default,
+            sources: ["\(appPath)/\(name)/UITests/**"],
+            resources: [],
+            dependencies: [
+                .target(name: "\(name)")
+            ])
         
-        return [mainTarget, testTarget, snapshotTestsTarget] // uiTestTarget
+        return [mainTarget, testTarget, snapshotTestsTarget, uiTestTarget]
     }
     
     public static func makeSchemes(targetName: String) -> [Scheme] {
