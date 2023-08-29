e9b7532d963a50574959ce408e17134c7b26bee5
Ronan O Ciosoig <ronan.ociosoig@edreamsodigeo.com>
2023-08-29 13:28:42 +0200

    Refactor subsystem string in log definitions

--- App/Pokedex/Sources/Core/Log.swift
+++ App/Pokedex/Sources/Core/Log.swift
@@ -9,9 +9,10 @@
 import Foundation
 import os.log
 
-public struct Log {
-    public static var general = OSLog(subsystem: "com.sonomos.pokedex", category: "general")
-    public static var network = OSLog(subsystem: "com.sonomos.pokedex", category: "network")
-    public static var data = OSLog(subsystem: "com.sonomos.pokedex", category: "data")
-}
+let subsystem = "com.sonomos.pokedex"
 
+public struct Log {
+    public static var general = OSLog(subsystem: subsystem, category: "general")
+    public static var network = OSLog(subsystem: subsystem, category: "network")
+    public static var data = OSLog(subsystem: subsystem, category: "data")
+}
