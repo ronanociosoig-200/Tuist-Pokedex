d40492c1808132bd82b798e0239d91f42b28011d
Ronan O Ciosoig <ronan.ociosoig@edreamsodigeo.com>
2023-10-07 10:00:38 +0200

    Disable the Widget extension because it requires Xcode 15 which is not currently working correctly

--- Project.swift
+++ Project.swift
@@ -10,7 +10,7 @@ let project = Project.app(name: "Pokedex",
                           externalDependencies: makeExternalDependencies(),
                           moduleTargets: makeAllCoreModules() + makeAllFeatures(),
                           testTargets: [.uiTests, .unitTests],
-                          additionalTargets: [.widgetExtension])
+                          additionalTargets: [])
 
 func makeExternalDependencies() -> [String] {
     return ["JGProgressHUD", "SnapshotTesting"]
