b759e7520593d51c2cef561fd80914456b8960e4
Ronan O Ciosoig <ronan.ociosoig@edreamsodigeo.com>
2023-12-30 16:54:02 +0000

    Split code coverage from the parsing and run the coverage before the cleanup

--- scripts/codeCoverageFromXCCov.sh
+++ scripts/codeCoverageFromXCCov.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-xcrun xccov view --report --json output/tests.xcresult > output/xccov_coverage.json
+# xcrun xccov view --report --json output/tests.xcresult > output/xccov_coverage.json
 swift scripts/parse-xccov-json-coverage.swift output/xccov_coverage.json terse
