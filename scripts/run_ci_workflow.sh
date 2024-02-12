633a4d4acb6c2fd46ad5eb0c3d9ba128d1b18985
Ronan O Ciosoig <ronan.ociosoig@edreamsodigeo.com>
2024-02-13 00:13:20 +0100

    Add dynamicWorkflows script Extend run_ci_workflow script to have a parameter and Add command to move the Workspace.swift file before running tests for test coverage Remove Workspace.swift

--- scripts/run_ci_workflow.sh
+++ scripts/run_ci_workflow.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+BUILD_ONLY=$1
 BRANCH=`git branch 2> /dev/null | sed -n -e 's/^\* \(.*\)/\1/p'`
 TOKEN="YOUR_PERSONAL_API_TOKEN"
 VCS="github"
 USER="ronanociosoig-200"
 PROJECT="Tuist-Pokedex"
+
+if [ "$BUILD_ONLY" = "build-only-workflow" ]; then
+  BUILD_AND_TEST_WORKFLOW="false"
+  BUILD_ONLY_WORKFLOW="true"
+else 
+  BUILD_AND_TEST_WORKFLOW="true"
+  BUILD_ONLY_WORKFLOW="false"
+fi
 
 curl --request POST \
   --url https://circleci.com/api/v2/project/$VCS/$USER/$PROJECT/pipeline \
@@ -12,6 +21,6 @@ curl --request POST \
   --header 'content-type: application/json' \
   --data '{
     "branch": "'$BRANCH'",
-    "parameters":{"build-and-test-workflow":false,
+    "parameters":{"build-and-test-workflow":'$BUILD_AND_TEST_WORKFLOW',
     "build-main-and-test-workflow":false,
-    "build-only-workflow":true}}'
+    "build-only-workflow":'$BUILD_ONLY_WORKFLOW'}}'
